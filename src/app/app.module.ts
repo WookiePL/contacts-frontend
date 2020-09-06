6d99c090102047644a319f6304a74b4175a4c2c3
Łukasz Janas <wookiepl@outlook.com>
2020-09-07 00:43:34 +0200

    Added HTML bootstrap. Working list of contacts.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'contacts', component: ContactListComponent},
+      { path: '', redirectTo: 'contacts', pathMatch: 'full'}
+    ])
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/models/contact.ts
@@ -0,0 +1,10 @@
+export class Contact {
+  contactId: number;
+  firstname: string;
+  lastname: string;
+  address: string;
+  email: string;
+  phoneNumber: string;
+  skills: string;
+  user: string;
+}
